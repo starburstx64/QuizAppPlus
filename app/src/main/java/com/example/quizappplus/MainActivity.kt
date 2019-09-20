93d3c0c4d4e0dc9e0fe4677ee6f6b7f663870491
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-09-20 13:53:39 -0500

    cambio de nombre de menu principal

--- app/src/main/java/com/example/quizappplus/MainActivity.kt
+++ app/src/main/java/com/example/quizappplus/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu_principal)
+        setContentView(R.layout.activity_menu_principal)
     }
 }
