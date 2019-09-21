3030345e122f315a83bd78e8c5b75e4534ff60d2
herna <hernan98_v@hotmail.com>
2019-09-20 21:21:25 -0500

    creacion de los activitys y sus widgets

---
+++ app/src/main/java/com/example/quizappplus/PuntuacionFinalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizappplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PuntuacionFinalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puntuacion_final)
+    }
+}
