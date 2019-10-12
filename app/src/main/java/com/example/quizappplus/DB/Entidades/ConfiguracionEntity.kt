0b0e9a631810f27375f640785a4f6d3ccc492df4
Victor <hernan98_v@hotmail.com>
2019-10-12 17:30:19 -0500

    Entidades Configuracion y Pregunta

--- app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
@@ -1,8 +1,16 @@
 package com.example.quizappplus.DB.Entidades
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "Configuracion")
 class ConfiguracionEntity (
-
+    @PrimaryKey @ColumnInfo(name = "idConfiguracion") val idConfiguracion : Int,
+    @ColumnInfo(name = "categoriasUsadas") val categoriasUsadas : String,
+    @ColumnInfo(name = "numeroPreguntas") val numeroPreguntas : Int,
+    @ColumnInfo(name = "dificultad") val dificultad : String,
+    @ColumnInfo(name = "pistasEnabled") val pistasEnabled : String,
+    @ColumnInfo(name = "noPistas") val noPistas : Short
 )
