7f4068e068c1e6a0ded1256f311fa11dc7fd8bc2
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-10-11 19:05:53 -0500

    Configuracion de librerias
    Agregue la configuracion de las librerias stetho y room, ya estan listas para usarse

---
+++ app/src/main/java/com/example/quizappplus/DB/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.quizappplus.DB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase:RoomDatabase(){
+
+}
