795dca46e23eb254137819f5e068a92a44c33fc3
Victor <hernan98_v@hotmail.com>
2019-10-12 17:31:52 -0500

    Entidades Configuracion y Pregunta

--- app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
@@ -2,7 +2,6 @@ package com.example.quizappplus.DB.Entidades
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Configuracion")
