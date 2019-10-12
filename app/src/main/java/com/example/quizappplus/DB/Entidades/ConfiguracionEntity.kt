e93bc3e0f63d81e1ea12bf8a0cd09b595aec6237
Victor <hernan98_v@hotmail.com>
2019-10-12 17:32:47 -0500

    Entidades Configuracion y Pregunta

--- app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/ConfiguracionEntity.kt
@@ -10,6 +10,6 @@ class ConfiguracionEntity (
     @ColumnInfo(name = "categoriasUsadas") val categoriasUsadas : String,
     @ColumnInfo(name = "numeroPreguntas") val numeroPreguntas : Int,
     @ColumnInfo(name = "dificultad") val dificultad : String,
-    @ColumnInfo(name = "pistasEnabled") val pistasEnabled : String,
-    @ColumnInfo(name = "noPistas") val noPistas : Short
+    @ColumnInfo(name = "pistasEnabled") val pistasEnabled : Short,
+    @ColumnInfo(name = "numeroPistas") val numeroPistas : Int
 )
