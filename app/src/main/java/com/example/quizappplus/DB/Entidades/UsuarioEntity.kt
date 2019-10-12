b253edae4e8531a9a26dfa27de80ed3514d0fedc
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-10-12 15:28:29 -0500

    Implementación de entities
    Implemente las entidades PreguntaJuego y Usuario

--- app/src/main/java/com/example/quizappplus/DB/Entidades/UsuarioEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/UsuarioEntity.kt
@@ -1,8 +1,28 @@
 package com.example.quizappplus.DB.Entidades
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-@Entity
-class UsuarioEntity (
-
+@Entity(tableName = "Usuario",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = ConfiguracionEntity::class,
+            parentColumns = arrayOf("idConfiguracion"),
+            childColumns = arrayOf("idConfiguracion"),
+            onDelete = ForeignKey.NO_ACTION),
+        ForeignKey(
+            entity = AplicacionEntity::class,
+            parentColumns = arrayOf("idAplicacion"),
+            childColumns = arrayOf("idAplicacion"),
+            onDelete = ForeignKey.NO_ACTION)
+    )
+)
+data class UsuarioEntity (
+    @PrimaryKey @ColumnInfo(name = "idUsuario") val idUsuario:Int,
+    @ColumnInfo(name = "idConfiguracion") val idConcifuracion:Int,
+    @ColumnInfo(name = "idAplicacion") val idAplicacion:Int,
+    @ColumnInfo(name = "imagenAvatar") var imagenAvatar:Int,
+    @ColumnInfo(name = "contraseña") var contraseña:String
 )
