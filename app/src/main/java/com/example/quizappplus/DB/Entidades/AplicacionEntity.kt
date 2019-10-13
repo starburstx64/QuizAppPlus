f1546ef637303209f71d0f967e849e705619a396
KyoZeta <47518944+KyoZeta@users.noreply.github.com>
2019-10-12 22:50:59 -0500

    implementacion de entities(Aplicacion, Categoria, Juego)

--- app/src/main/java/com/example/quizappplus/DB/Entidades/AplicacionEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/AplicacionEntity.kt
@@ -1,8 +1,15 @@
 package com.example.quizappplus.DB.Entidades
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-@Entity()
-class AplicacionEntity(
-
+@Entity(foreignKeys = arrayOf(ForeignKey(entity = UsuarioEntity::class,
+    parentColumns = arrayOf("idUsuario"),
+    childColumns = arrayOf("idUsuarioActivo")))
+)
+data class AplicacionEntity(
+    @PrimaryKey(autoGenerate = true)
+    var idApliacion : Int,
+    var idUsuarioActivo : Int
 )
