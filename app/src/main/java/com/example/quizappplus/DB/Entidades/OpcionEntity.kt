63f6cebebd65f198bd6b3e5f88347968c6d51e85
David Segovia Ley <starburstx64@gmail.com>
2019-10-12 19:00:08 -0500

    Implementados OpcionEntity y PuntuacionEntity

--- app/src/main/java/com/example/quizappplus/DB/Entidades/OpcionEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/OpcionEntity.kt
@@ -1,8 +1,19 @@
 package com.example.quizappplus.DB.Entidades
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-@Entity
-class OpcionEntity (
-
+@Entity(foreignKeys = arrayOf(
+                        ForeignKey(
+                            entity = PreguntaEntity::class,
+                            parentColumns = arrayOf("idPregunta"),
+                            childColumns = arrayOf("idPregunta"),
+                            onDelete = ForeignKey.NO_ACTION))
+)
+data class OpcionEntity (
+    @PrimaryKey(autoGenerate = true) val idOpcion : Int,
+    val texto : String,
+    val isCorrect : Boolean,
+    val idPregunta : Int
 )
