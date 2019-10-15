63c57cd21f68d935121faebb09c6c0a75e9e99ef
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-10-14 19:51:20 -0500

    Update CategoriaEntity.kt

--- app/src/main/java/com/example/quizappplus/DB/Entidades/CategoriaEntity.kt
+++ app/src/main/java/com/example/quizappplus/DB/Entidades/CategoriaEntity.kt
@@ -5,8 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Categoria")
-
 data class CategoriaEntity(
-    @PrimaryKey @ColumnInfo(name = "idCategoria") val idCategoria : Int,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "idCategoria") val idCategoria : Int,
     @ColumnInfo(name = "nombre") val nombre : String
-    )
+)
