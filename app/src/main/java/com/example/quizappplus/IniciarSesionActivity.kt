47e815522f0daf9d87557757d4c9b7f5d4373762
KyoZeta <47518944+KyoZeta@users.noreply.github.com>
2019-10-20 15:56:31 -0500

    Creacion de Layouts "Registrar usuario" y "Iniciar sesion"

---
+++ app/src/main/java/com/example/quizappplus/IniciarSesionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizappplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IniciarSesionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+    }
+}
