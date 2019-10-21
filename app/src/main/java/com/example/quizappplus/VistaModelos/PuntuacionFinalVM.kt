7230a441d6fd437f00c57ac2c5a5117fb6afae25
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-10-21 16:42:46 -0500

    Agregado de puntuacion final etc etc
    Agregue que al terminar un juego te sale la puntuacion final y cambie el inicio de sesion cuando creas un nuevo usuario

--- app/src/main/java/com/example/quizappplus/VistaModelos/PuntuacionFinalVM.kt
+++ app/src/main/java/com/example/quizappplus/VistaModelos/PuntuacionFinalVM.kt
@@ -9,19 +9,16 @@ import com.example.quizappplus.Modelos.Usuario
 class PuntuacionFinalVM:ViewModel() {
     private var listaPuntuaciones:List<ReportesDao.PuntuacionReport> = listOf()
     private var flagVentanaAbierta=false
-    private lateinit var usuarioActual: Usuario
+    var PuntuacionUsuario:Int = 0
+    var Acuracy:Double = 0.0
 
     val ListaPuntuaciones get() = listaPuntuaciones
-    val JugadorActual get() = usuarioActual
 
     fun SetJugadores(db:AppDatabase){
         if (!flagVentanaAbierta) {
             this.listaPuntuaciones = db.getReportesDao().GetPuntuacionesReport()
             VentanaAbierta()
         }
-    }
-    fun SetJugadorActual(usuario: Usuario){
-        usuarioActual=usuario
     }
 
     fun VentanaAbierta() {flagVentanaAbierta=true}
